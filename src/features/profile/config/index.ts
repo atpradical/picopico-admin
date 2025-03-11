7d2e6510a2fc16497711fcd0038f40f9d82be146
atpradical <ivan.dolgikh1199@gmail.com>
2025-03-11 23:21:35 +0300

    Remove unused public-user components and redundant constants
    Deleted outdated components and styles related to public-user UI, such as TotalUsers. Additionally, removed unused constants like POSTS_HOME_PAGE_SIZE and others from profile and public-user configurations to improve code maintainability.

--- src/features/profile/config/index.ts
+++ src/features/profile/config/index.ts
@@ -1,7 +1,4 @@
 export const ABOUT_ME_REGEX = /^[0-9a-zA-ZА-Яа-я!"#$%&'()*+,\-.:;<=>?@[\]^_`{|}~\s]*$/
 export const MAX_ABOUT_ME_LENGTH = 200
 export const MIN_USER_AGE = 13
-export const MAX_CITY_POPULATION = 500000
-export const AVATAR_MAX_FILE_SIZE = 10000000 // 10MB in bytes
-export const ALLOWED_IMAGE_UPLOAD_TYPES = ['image/jpeg', 'image/png']
 export const INITIAL_CURSOR = 0
--- src/features/public-user/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const POSTS_HOME_PAGE_SIZE = 4
