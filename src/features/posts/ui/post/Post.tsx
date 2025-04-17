a190d610ed75a77dc7cb5e94fb8ce3ce9c84f167
atpradical <ivan.dolgikh1199@gmail.com>
2025-04-17 11:50:26 +0300

    feat(posts): enable user ban/unban functionality

--- src/features/posts/ui/post/Post.tsx
+++ src/features/posts/ui/post/Post.tsx
@@ -1,7 +1,12 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, useCallback, useState } from 'react'
+import { useFormContext } from 'react-hook-form'
 
 import { MockPostImage, PostCarousel, PostDescription } from '@/features/posts/ui'
+import { AlertConfig, initialAlertConfig } from '@/features/users/config'
+import { BanUserForm } from '@/features/users/ui'
 import { GetAllPostsQuery } from '@/services/posts'
+import { useTranslation } from '@/shared/hooks'
+import { ConfirmDialog } from '@/shared/ui/components'
 import { getDateDistanceToNow } from '@/shared/utils'
 import {
   Avatar,
@@ -15,54 +20,106 @@ import { useRouter } from 'next/router'
 import s from './Post.module.scss'
 
 type PostProps = {
+  isLoading: boolean
+  onBanConfirm: () => void
+  onUnblockConfirm: () => void
   post: NonNullable<GetAllPostsQuery['getPosts']['items'][number]>
+  userFullName: string
 } & ComponentPropsWithoutRef<'div'>
 
 type PostPropsRef = ElementRef<'div'>
 
-export const Post = forwardRef<PostPropsRef, PostProps>(({ post, ...rest }, ref) => {
-  const { locale } = useRouter()
+export const Post = forwardRef<PostPropsRef, PostProps>(
+  ({ isLoading, onBanConfirm, onUnblockConfirm, post, userFullName, ...rest }, ref) => {
+    const { t } = useTranslation()
+    const { locale } = useRouter()
+    const { reset } = useFormContext()
+    const [alert, setAlert] = useState<AlertConfig>(initialAlertConfig)
+    const formattedCreatedAt = getDateDistanceToNow(new Date(post.createdAt), locale ?? 'en')
 
-  const formattedCreatedAt = getDateDistanceToNow(new Date(post.createdAt), locale ?? 'en')
+    const [expand, setExpand] = useState(false)
+    const toggleDescription = () => {
+      setExpand(!expand)
+    }
 
-  const [expand, setExpand] = useState(false)
-  const toggleDescription = () => {
-    setExpand(!expand)
-  }
+    const closeDialog = useCallback(() => {
+      setAlert(initialAlertConfig)
+      reset()
+    }, [reset])
 
-  const isBlock = !!post.userBan?.createdAt
+    const onUserBlockHandler = () => {
+      setAlert({
+        bodyElement: <BanUserForm />,
+        isOpen: true,
+        onConfirm: () => {
+          onBanConfirm()
+          reset()
+          closeDialog()
+        },
+        translations: {
+          ...t.usersPage.blockUserDialog,
+          visibleBody: t.usersPage.blockUserDialog.visibleBody + ' ' + userFullName + '?',
+        },
+      })
+    }
 
-  return (
-    <div className={s.publicationContainer} {...rest} ref={ref}>
-      {post.images?.[0]?.url ? <PostCarousel expand={expand} post={post} /> : <MockPostImage />}
-      <div className={s.avatarContainer}>
-        <Avatar
-          showFallback
-          showUserName
-          size={'xs'}
-          src={post.postOwner.avatars?.[0]?.url ?? ''}
-          userName={post.postOwner.userName}
+    const onUserUnblockHandler = () => {
+      setAlert({
+        bodyElement: null,
+        isOpen: true,
+        onConfirm: () => {
+          onUnblockConfirm()
+          closeDialog()
+        },
+        translations: {
+          ...t.usersPage.unblockUserDialog,
+          visibleBody: t.usersPage.unblockUserDialog.visibleBody + ' ' + userFullName + '?',
+        },
+      })
+    }
+
+    const isBlock = !!post.userBan?.createdAt
+
+    return (
+      <div className={s.publicationContainer} {...rest} ref={ref}>
+        {post.images?.[0]?.url ? <PostCarousel expand={expand} post={post} /> : <MockPostImage />}
+        <div className={s.avatarContainer}>
+          <Avatar
+            showFallback
+            showUserName
+            size={'xs'}
+            src={post.postOwner.avatars?.[0]?.url ?? ''}
+            userName={post.postOwner.userName}
+          />
+          {isBlock ? (
+            <>
+              <Button onClick={onUserUnblockHandler} title={'unban user'} variant={'icon'}>
+                <BlockIcon />
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={onUserBlockHandler} title={'ban user'} variant={'icon'}>
+                <RadioButtonUncheckedIcon />
+              </Button>
+            </>
+          )}
+        </div>
+        <div>
+          <Typography grey variant={'small'}>
+            {formattedCreatedAt}
+          </Typography>
+          <PostDescription expand={expand} onToggle={toggleDescription} post={post} />
+        </div>
+        <ConfirmDialog
+          bodyElement={alert.bodyElement}
+          isLoading={isLoading}
+          isOpen={alert.isOpen}
+          onConfirm={alert.onConfirm}
+          onOpenChange={closeDialog}
+          t={alert.translations}
         />
-        {isBlock ? (
-          <>
-            <Button title={'unban user'} variant={'icon'}>
-              <BlockIcon />
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button title={'ban user'} variant={'icon'}>
-              <RadioButtonUncheckedIcon />
-            </Button>
-          </>
-        )}
       </div>
-      <div>
-        <Typography grey variant={'small'}>
-          {formattedCreatedAt}
-        </Typography>
-        <PostDescription expand={expand} onToggle={toggleDescription} post={post} />
-      </div>
-    </div>
-  )
-})
+    )
+  }
+)
--- src/views/posts/PostsPage.tsx
+++ src/views/posts/PostsPage.tsx
@@ -1,14 +1,18 @@
 import { useContext, useEffect, useRef, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 import { Post } from '@/features/posts/ui'
 import { INITIAL_CURSOR } from '@/features/users/config'
+import { BanUserFormFields, banUserSchemeCreator } from '@/features/users/model'
 import { useGetAllPostsQuery } from '@/services/posts'
 import { OnPostAddedSubscription, POST_ADDED_SUBSCRIPTION } from '@/services/posts/subscriptions'
 import { InputMaybe, QueryGetPostsArgs, QueryGetUsersArgs } from '@/services/schema.types'
+import { useBanUserMutation, useUnbanUserMutation } from '@/services/users'
 import { AuthContext } from '@/shared/context'
 import { useSearch, useTranslation } from '@/shared/hooks'
 import { Page, getNavigationLayout } from '@/shared/ui/layout'
 import { Spinner, TextField } from '@atpradical/picopico-ui-kit'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useIntersectionObserver } from 'usehooks-ts'
 
@@ -21,6 +25,14 @@ function PostsPage() {
   const { isReady, query } = useRouter()
   const searchTerm = query.searchTerm ? query.searchTerm : ''
   const [fetching, setFetching] = useState(false)
+
+  const methods = useForm<BanUserFormFields>({
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(banUserSchemeCreator(t.validation)),
+  })
+
+  const { handleSubmit } = methods
 
   const { data, fetchMore, loading, subscribeToMore } = useGetAllPostsQuery({
     skip: !isReady || !isAuth,
@@ -96,29 +108,88 @@ function PostsPage() {
     }
   }, [isIntersecting, fetchMore, postsList, searchTerm])
 
+  const [banUserMutation, { loading: LoadingBan }] = useBanUserMutation({})
+
+  const [unbanUserMutation, { loading: loadingUnBan }] = useUnbanUserMutation({})
+
   const isLoading = loading || fetching
 
   return (
-    <Page pt={'36px'}>
-      <div className={s.container}>
-        <TextField
-          label={t.postPage.searchLabel}
-          onChange={searchChangeHandler}
-          onClear={clearSearchHandler}
-          placeholder={t.postPage.searchPlaceholder}
-          value={searchTerm}
-          variant={'search'}
-        />
-        <section className={s.postsContainer}>
-          {postsList?.map((post, index) => {
-            const lastPost = index === postsList.length - 1
+    <>
+      <Page pt={'36px'}>
+        <div className={s.container}>
+          <TextField
+            label={t.postPage.searchLabel}
+            onChange={searchChangeHandler}
+            onClear={clearSearchHandler}
+            placeholder={t.postPage.searchPlaceholder}
+            value={searchTerm}
+            variant={'search'}
+          />
+          <section className={s.postsContainer}>
+            {postsList?.map((post, index) => {
+              const lastPost = index === postsList.length - 1
 
-            return <Post key={post.id} post={post} ref={lastPost ? lastPostRef : null} />
-          })}
-        </section>
-        {isLoading && <Spinner label={t.loading} />}
-      </div>
-    </Page>
+              const userFullName = post.postOwner.firstName
+                ? post.postOwner.firstName + ' ' + post.postOwner.lastName
+                : post.postOwner.userName
+
+              const handleBanUser = handleSubmit(async data => {
+                const finalBanReason = data.customReason ? data.customReason : data.reason
+
+                await banUserMutation({
+                  update: cache => {
+                    cache.modify({
+                      fields: {
+                        userBan(existingUserBan = {}) {
+                          return {
+                            ...existingUserBan,
+                            createdAt: new Date().toISOString(),
+                            reason: finalBanReason,
+                          }
+                        },
+                      },
+                      id: cache.identify({ __typename: 'Post', id: post.id }),
+                    })
+                  },
+                  variables: { banReason: finalBanReason, userId: post.postOwner.id },
+                })
+              })
+
+              const handleUnbanUser = async () => {
+                await unbanUserMutation({
+                  update: cache => {
+                    cache.modify({
+                      fields: {
+                        userBan() {
+                          return null
+                        },
+                      },
+                      id: cache.identify({ __typename: 'Post', id: post.id }),
+                    })
+                  },
+                  variables: { userId: post.postOwner.id },
+                })
+              }
+
+              return (
+                <FormProvider key={post.id} {...methods}>
+                  <Post
+                    isLoading={loading || LoadingBan || loadingUnBan}
+                    onBanConfirm={handleBanUser}
+                    onUnblockConfirm={handleUnbanUser}
+                    post={post}
+                    ref={lastPost ? lastPostRef : null}
+                    userFullName={userFullName}
+                  />
+                </FormProvider>
+              )
+            })}
+          </section>
+          {isLoading && <Spinner label={t.loading} />}
+        </div>
+      </Page>
+    </>
   )
 }
 
